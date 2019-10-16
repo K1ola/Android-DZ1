1bf90d5f53c85cf386cad3339ca5dd48a70914ec
kate <kirilek@bk.ru>
2019-10-17 02:24:35 +0300

    added db, broke logic

--- app/src/main/java/com/example/myapplication/ListFragment.java
+++ app/src/main/java/com/example/myapplication/ListFragment.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +16,11 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 import java.util.ArrayList;
 
 public class ListFragment extends Fragment implements ItemAdapter.OnNumberClickListener {
@@ -22,6 +30,32 @@ public class ListFragment extends Fragment implements ItemAdapter.OnNumberClickL
     private ItemAdapter itemAdapter;
 
     private ArrayList<Integer> data = new ArrayList<>();
+
+    //////////////////////////////////
+    class DBHelper extends SQLiteOpenHelper {
+        final String CREATE_TABLE = "CREATE TABLE data (count int)";
+        final String DB_NAME = "mySuperDB.db";
+        Context mContext;
+
+        public DBHelper(Context context, int dbVer){
+            super(context, "mySuperDB.db", null, dbVer);
+            mContext = context;
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_TABLE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            //проверяете какая версия сейчас и делаете апдейт
+            db.execSQL("DROP TABLE IF EXISTS tableName");
+            onCreate(db);
+        }
+	}
+
+    DBHelper dbHelper;
 
     public ListFragment() {
         for (int i = 0; i < 100; i++) {
@@ -39,43 +73,76 @@ public class ListFragment extends Fragment implements ItemAdapter.OnNumberClickL
 
     @Override
     public void onViewCreated(@NonNull View view, @NonNull Bundle savedInstanceState) {
+
         if (savedInstanceState != null) {
             data = savedInstanceState.getIntegerArrayList(key_data);
         }
-        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
 
-        int orientation = view.getContext().getResources().getConfiguration().orientation;
-        final int columnsNumber = orientation == Configuration.ORIENTATION_LANDSCAPE ? 4 : 3;
+        dbHelper = new DBHelper(view.getContext(), 1);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
 
-        recyclerView.setLayoutManager(new GridLayoutManager(view.getContext(), columnsNumber));
+        Cursor c = db.query("data", null, null, null, null, null, null);
+        if (c.moveToFirst()) {
 
-        itemAdapter = new ItemAdapter(data, this, view.getContext());
-        recyclerView.setAdapter(itemAdapter);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
+            // определяем номера столбцов по имени в выборке
+            int Col = c.getColumnIndex("count");
+            int lastNum;
+            do {
+                // получаем значения по номерам столбцов и пишем все в лог
+                lastNum = c.getInt(Col);
+                // переход на следующую строку
+                // а если следующей нет (текущая - последняя), то false - выходим из цикла
+            } while (c.moveToNext());
 
-        Button button = view.findViewById(R.id.addButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                data.add(itemAdapter.getItemCount() + 1);
-                itemAdapter.SetItemCount(data);
-            }
-        });
-    }
 
-    @Override
-    public void onNumberClick(int number, int color) {
-        if (getActivity() == null || !(getActivity() instanceof ItemAdapter.OnNumberClickListener)) {
-            return;
+            data.add(lastNum);
         }
 
-        ((ItemAdapter.OnNumberClickListener) getActivity()).onNumberClick(number, color);
+            RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+
+            int orientation = view.getContext().getResources().getConfiguration().orientation;
+            final int columnsNumber = orientation == Configuration.ORIENTATION_LANDSCAPE ? 4 : 3;
+
+            recyclerView.setLayoutManager(new GridLayoutManager(view.getContext(), columnsNumber));
+
+            itemAdapter = new ItemAdapter(data, this, view.getContext());
+            recyclerView.setAdapter(itemAdapter);
+            recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+            Button button = view.findViewById(R.id.addButton);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    data.add(itemAdapter.getItemCount() + 1);
+                    itemAdapter.SetItemCount(data);
+                }
+            });
+
+//            dbHelper.close();
+
     }
 
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle state) {
-        super.onSaveInstanceState(state);
-        state.putIntegerArrayList(key_data, data);
+        @Override
+        public void onNumberClick ( int number, int color){
+            if (getActivity() == null || !(getActivity() instanceof ItemAdapter.OnNumberClickListener)) {
+                return;
+            }
+
+            ((ItemAdapter.OnNumberClickListener) getActivity()).onNumberClick(number, color);
+        }
+
+        @Override
+        public void onSaveInstanceState (@NonNull Bundle state){
+            super.onSaveInstanceState(state);
+            state.putIntegerArrayList(key_data, data);
+
+
+            ContentValues cv = new ContentValues();
+            cv.put("count", data.size());
+
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            // вставляем запись и получаем ее ID
+            db.insert("data", null, cv);
+        }
     }
 
-}
